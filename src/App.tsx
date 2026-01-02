53fafe6034d4667740f9c7a19e24c17934445ffb
zeerak1 <zeerak@angelicinsurance.com.au>
2026-01-02 23:41:25 +0500

    Added routing, supabase signup basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
